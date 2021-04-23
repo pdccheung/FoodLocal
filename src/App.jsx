a872472d69d04149c8d64aa0f5d35f20126588ab
pdccheung <pdccheung@gmail.com>
2021-04-23 17:41:11 -0400

    basic app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        App
+      </main>
+    )
+  }
+}
