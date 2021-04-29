1b36b6d1f64003e7566ec36a5cfac8e74ebf5413
pdccheung <pdccheung@gmail.com>
2021-04-29 17:49:07 -0400

    testing map feature

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -9,8 +9,8 @@ import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import HomePage from "../../pages/HomePage/Home";
 import Images from "../ImagesPage/ImagesPage";
-import { getCurrentLatLng } from "../../services/geolocation";
-import Map from "../MapPage/Map"
+import Map from "../../components/Map/Map"
+
 
 
 export default class App extends Component {
@@ -19,7 +19,9 @@ export default class App extends Component {
     foods: [],    
     lat: null,
     lng: null,
+    restaurantAddress: null,
   };
+
 
   setUserInState = (incomingUserData) => {
     this.setState({ user: incomingUserData });
@@ -32,8 +34,6 @@ export default class App extends Component {
       let userDoc = JSON.parse(atob(token.split(".")[1])).user; // decode jwt token
       this.setState({ user: userDoc });
     }
-    const {lat, lng} = await getCurrentLatLng();
-    this.setState({lat, lng});
   }
 
 
@@ -44,6 +44,57 @@ export default class App extends Component {
       .then((data) => this.setState({ foods: data }));
   };
 
+
+
+getLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.getCoordinates, this.handleLocationError);
+      } else {
+        alert("Geolocation is not supported by this browser.")
+      }
+    }
+
+getCoordinates = (position) => {
+
+  console.log(position.coords.latitude)
+  console.log(position.coords.longitude)
+    this.setState({
+      lat: position.coords.latitude, 
+      lng: position.coords.longitude
+    })
+    this.reverseGeocodeCoords()
+}
+
+reverseGeocodeCoords = () => {
+  fetch(``)
+  .then(response => {response.json()})
+  .then(data => console.log(data)
+    // this.setState({
+    // restaurantAddress: data.results[0].formatted_address
+  // })
+  )
+  .catch(error => alert(error))
+}
+
+handleLocationError(error){
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      alert ("User denied the request for Geolocation.")
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert ("Location information is unavailable.")
+      break;
+    case error.TIMEOUT:
+      alert("The request to get user location timed out.")
+      break;
+    case error.UNKNOWN_ERROR:
+      alert("An unknown error occurred.")
+      break;
+    default:
+      alert("An unknown error occurred.")
+
+  }
+}
   render() {
     return (
       <>
@@ -64,7 +115,7 @@ export default class App extends Component {
            
            <Route path="/login" render={(props) => <AuthPage {...props} setUserInState={this.setUserInState} /> } />
 
-           <Route path="/map" render={(props) => <Map lat={this.state.lat} lng={this.state.lng} />} />
+         
 
             {this.state.user ? (
               <Switch>
@@ -94,13 +145,15 @@ export default class App extends Component {
                     <Images {...props} />
                   )}
                 />
+                <Route path="/map" render={(props) => <Map  {...props} lat={this.state.lat} lng={this.state.lng}
+                 getCoordinates={this.getCoordinates} getLocation={this.getLocation}
+                 reverseGeocodeCoords={this.reverseGeocodeCoords} />} />
                 {/* and in case nothing matches, we redirect: */}
                 <Redirect to="/" />
               </Switch>
             ) : ( null
               // <AuthPage setUserInState={this.setUserInState} />
             )}
- <Map lat={this.state.lat} lng={this.state.lng} />
           </Container>
         </main>
         <Footer />
--- src/services/geolocation.js
+++
@@ -1,9 +0,0 @@
-export function getCurrentLatLng() {
-    // Wrap getCurrentPosition to return a promise
-    return new Promise(resolve => {
-      navigator.geolocation.getCurrentPosition(pos => resolve({
-        lat: pos.coords.latitude,
-        lng: pos.coords.longitude
-      }));
-    });
-  }
